8557e96dc2aefe65926fd9eb96dc94185594a519
Sourave halder <souravh093@gmail.com>
2023-11-06 22:57:58 +0600

    fix some proble

--- module2/src/interface.ts
+++ module2/src/interface.ts
@@ -5,8 +5,8 @@
     }
 
     // one type we include another type
-    type UserWithRole = User1 & { 
-        role: string 
+    type UserWithRole = User1 & {
+        role: string
     }
 
     const user3: UserWithRole = {
@@ -33,6 +33,27 @@
         name: "Hello",
         age: 23,
         Role: "UI/UX"
+    }
+
+
+
+    type Roll = number[];
+
+    const rollNumber1: Roll = [5, 6, 22, 9];
+
+    // using interface we can assign type as well
+    interface Roll2 {
+        [index: number]: number
+    }
+
+
+    type Add = (num1: number, num2: number) => number;
+    const add: Add = (num1, num2) => num1 + num2;
+
+
+    // using interface we can assign function also
+    interface Add2 {
+        (num1: number, num2: number): number
     }
 
 
